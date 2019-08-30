b6fee3280f7f245f7bf4a64c298264a58f3859e1
Yallison Melo <yallison@gmail.com>
2019-08-29 21:38:45 -0300

    Starting Project

---
+++ src/main/java/br/com/yfsm/theprojectspringboot/TheprojectspringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.yfsm.theprojectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheprojectspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheprojectspringbootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/domain/User.java
@@ -0,0 +1,34 @@
+package domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Table(name = "user")
+public class User {
+
+    @Id
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+    @NotBlank
+    private Boolean active = true;
+    @NotBlank
+    private Date dateInsert;
+    private Date dateUpdate;
+}
